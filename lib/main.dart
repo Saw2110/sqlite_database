99637bf80621019689395276811e172073767fcd
Sabin <sabin.ghimire1827@gmail.com>
2021-02-10 16:50:19 +0545

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sqlite_database/Database/databaseHelper.dart';
-import 'package:sqlite_database/Demo/helper.dart';
 import 'package:sqlite_database/index.dart';
-import 'package:sqlite_database/networkhelper.dart';
-import 'package:sqlite_database/sharePreference/sharePreference.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
